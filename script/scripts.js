f86e0fbbcf7f81885ed9108b23a36ec332fbd354
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-04-17 12:03:32 -0300

    add function for update the dolar in real time

--- script/scripts.js
+++ script/scripts.js
@@ -3,23 +3,22 @@ const graficoDolar = document.getElementById('graficoDolar');
 const graficoParaDolar = new Chart(graficoDolar, {
     type: 'line',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: [],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Dólar',
+        data: [],
         borderWidth: 1
       }]
     },
   });
-
-
+setInterval (() => conectaAPI(), 5000);
 async function conectaAPI() {
     const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
     const conectaTraduzido = await conecta.json();
-    console.log(conectaTraduzido);
+    let tempo = gerarHorario();
+    let valor = conectaTraduzido.USDBRL.ask;
+    adicionarDados(graficoParaDolar, tempo, valor)
 }
-
-setInterval (() => conectaAPI(), 5000);
 
 function gerarHorario () {
     let data = new Date();
@@ -28,4 +27,10 @@ function gerarHorario () {
     return horario
 }
 
-gerarHorario ();
+function adicionarDados (grafico, legenda, dados) {
+    grafico.data.labels.push(legenda);
+    grafico.data.datasets.forEach((dataset) => {
+        dataset.data.push(dados);
+    })
+    grafico.update();
+}
